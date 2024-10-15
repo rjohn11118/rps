abe5f3e8d807c32e72869f303a30f8afc94cc545
River <river.johnson@stu.barren.kyschools.us>
2024-10-15 08:14:11 -0500

    Initial Commit

---
+++ rps/rpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpsApp.swift
+//  rps
+//
+//  Created by Student on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct rpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
